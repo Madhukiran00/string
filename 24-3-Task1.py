7e0e1d547d3c028df5d494e998e5d91df84a59f9
Madhukiran00 <162177926+Madhukiran00@users.noreply.github.com>
2025-03-24 23:20:09 +0530

    Add files via upload

---
+++ 24-3-Task1.py
@@ -0,0 +1,25 @@
+str="   My name is madhu   "
+#upper
+print(str.upper())
+#lower
+print(str.lower())
+#strip
+print(str.strip())
+#lstrip
+print(str.lstrip())
+#rstrip
+print(str.rstrip())
+#replace
+print(str.replace("madhu","madhukiran"))
+#startswith
+print(str.startswith("  "))
+#endswith
+print(str.endswith("  "))
+#find()
+print(str.find("a"))
+#count
+print(str.count("a"))
+#title
+print(str.title())
+#capitalize
+print(str.capitalize())
